c51c8fbfbfc9ac536fd5178633077d38a0c29055
cdr <514426002@qq.com>
2018-05-05 17:02:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/callbackhandler/MyButton.java
@@ -0,0 +1,22 @@
+package com.example.callbackhandler;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.support.v7.widget.AppCompatButton;
+
+public class MyButton extends AppCompatButton{
+    public MyButton(Context context , AttributeSet set){
+        super(context , set);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        super.onKeyDown(keyCode , event);
+        Log.v("com.sise" , "the onKeyDown in MyButton");
+        //返回true，表明该事件不会向外扩散
+        return true;
+    }
+}
+
+
